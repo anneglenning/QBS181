41daca7b74bae506ed30a257161dece7028948aa
sabinhart <sabin@sabinhart.com>
2024-11-17 19:51:26 -0500

    missing vals analysis

--- Scripts/SabinAnalysis.R
+++ Scripts/SabinAnalysis.R
@@ -9,21 +9,11 @@ library(tidyverse)
 
 setwd("~/Desktop/Data Wrangling/Final Project")
 
-load("final_data.Rdata")
+load("Data/final_data.Rdata")
 
 
-# Analysis ----------------------------------------------------------------
+# Missing Data Analysis ---------------------------------------------------
 
-d <- county_data %>% 
-  filter(!is.na(life_expectancy_2020)) %>% 
-  group_by(State) %>% 
-  mutate(ha_corr = cor(life_expectancy_2020, median_aqi)) %>% 
-  summarize(correlation = mean(ha_corr))
-
-
-# Data Patching -----------------------------------------------------------
-
-load("2020data.Rdata")
 
 
 
