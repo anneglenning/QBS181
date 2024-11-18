bb3225b97604fc5237366b8ed0cf169be5dd3ff5
sabinhart <sabin@sabinhart.com>
2024-11-17 19:53:57 -0500

    forgot to save my work before pushing lol

--- Scripts/SabinAnalysis.R
+++ Scripts/SabinAnalysis.R
@@ -14,6 +14,23 @@ load("Data/final_data.Rdata")
 
 # Missing Data Analysis ---------------------------------------------------
 
+# view missing values by row and col
+sort(colSums(is.na(county_data)), decreasing=T)
+hist(colSums(is.na(county_data)), main = "# of missing values by column",xlab="NAs")
 
+sort(rowSums(is.na(county_data)), decreasing=T)
+hist(rowSums(is.na(county_data)), main = "# of missing values by row",xlab="NAs")
+
+# plots show that vast majority of cols/rows are complete
+
+# subset into
+na_counties <- county_data[rowSums(is.na(county_data)) > 0, ]
+
+complete_counties <- county_data[rowSums(is.na(county_data)) == 0, ]
+
+sum(complete_counties$population_2020)/(sum(complete_counties$population_2020) + sum(na_counties$population_2020))
+
+# > 94% of the population remains with only complete cases so we are going to 
+# ignore missing values and let the default behavior of row removal be sufficient
 
 
